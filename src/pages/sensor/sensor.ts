6cc79747c71c19996b0baacc7ff731e355432651
Andrzej Borzęcki <Andrzej Borzcki>
2017-02-12 17:28:36 +0100

    SockJS + Stomp + Typings, setInterval problem

--- src/pages/sensor/sensor.ts
+++ src/pages/sensor/sensor.ts
@@ -2,8 +2,9 @@ import { Configuration } from './../../shared/app.configuration';
 import { Component } from '@angular/core';
 import { NavController,NavParams } from 'ionic-angular';
 import { Sensor } from '../../model/sensor';
-import { $WebSocket } from 'angular2-websocket/angular2-websocket'
 import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs';
+
 
 
 /*
@@ -24,14 +25,16 @@ export class SensorPage {
   
   public sensor: Sensor;
   stompClient: any;
-  public ws: $WebSocket;
+
   
 
 
   constructor(public navCtrl: NavController,public navParams: NavParams,_configuration: Configuration) {
     this.sensor=this.navParams.get('param1');    
-    this.ws = new $WebSocket("ws://"+_configuration.BaseURL+"/websockets");
-    this.stompClient = Stomp.over(this.ws);
+    var socket = new SockJS(_configuration.BaseURL+"/websockets");
+    this.stompClient = Stomp.over(socket);
+    console.log(this.stompClient);
+    console.log(socket);
     this.stompClient.connect({}, function (frame) {
         console.log("Connected: " + frame);
         this.stompClient.subscribe("/home1/temperature", function (msg) {
@@ -51,7 +54,6 @@ export class SensorPage {
 
   ionViewWillLeave(){
     this.stompClient.disconnect();
-    this.ws.close();
   }
 
 }
